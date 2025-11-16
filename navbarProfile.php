f17859c0ac881294692a351c32940d980b0b3ae9
EstevaoLocks <estevao.locks11@gmail.com>
2025-11-16 20:48:56 -0300

    criando estrutura página do perfil

--- navbarProfile.php
+++ navbarProfile.php
@@ -1,27 +1,25 @@
 <!-- Menu -->
-<nav class="menu">
-    <div class="logo">
-        <div class="logo-img">
+<nav class="menu-pgProfile">
+    <div class="imgPerfil-pgProfileNav">
             <img src="assets/img/Logo Azul.png" alt="">
-        </div>
         <div class="nome-site">
-            <p>SportMatch</p>
-            <p class="azul">Brasil</p>
+            <p>Bem vindo,</p>
+            <p class="azul">Fulano de Tal</p>
         </div>
     </div>
 
-    <div class="links">
+    <div class="links-pgProfileNav">
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="anuncioQuadra.php">Registrar</a></li>
-            <li><a href="profile.php">Categorias</a></li>
+            <li><a href="#">Categorias</a></li>
             <li><a href="#">Perto de você</a></li>
             <li><a href="#">Por que escolher?</a></li>
             <li><a href="#">Contato</a></li>
         </ul>
     </div>
 
-    <div class="links">
+    <div class="links-pgProfileNav">
         <div>
             <a class="btn-secondary1-menu" href="#">Login<img id="iconePersonBtnMenu" class="iconePerson-BtnMenu" src="assets/img/icons/iconePerson.svg" alt=""></a>
         </div>
