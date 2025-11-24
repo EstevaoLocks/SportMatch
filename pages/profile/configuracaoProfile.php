5d752aad4c802b8b6308abf7bdf194f96538d2f5
EstevaoLocks <estevao.locks11@gmail.com>
2025-11-24 03:36:15 -0300

    concluindo update dados usuario

--- pages/profile/configuracaoProfile.php
+++ pages/profile/configuracaoProfile.php
@@ -2,6 +2,32 @@
     //chama arquivo que define raíz do projeto
     require_once __DIR__ . '/../../config.php';
     require_once BASE_PATH . '/sistema/classes/Usuario.php';
+    require_once BASE_PATH . '/sistema/conexao.php';
+
+    $sql = $pdo->prepare("SELECT * FROM usuario WHERE cod_usuario = :id LIMIT 1");
+    $sql->bindValue(":id", $_SESSION['cod_usuario']);
+    $sql->execute();
+
+    $usuario = $sql->fetch(PDO::FETCH_ASSOC);
+
+    if (!$usuario) {
+        die("Usuário não encontrado.");
+    }
+
+    $username = $usuario['username'] ?? '';
+    $senha = $usuario['senha'] ?? '';
+    $nome = $usuario['nome'] ?? '';
+    $rg = $usuario['rg'] ?? '';
+    $cpf = $usuario['cpf'] ?? '';
+    $email = $usuario['email'] ?? '';
+    $datanasc = $usuario['datanasc'] ?? '';
+    $telefone = $usuario['telefone'] ?? '';
+    $cep = $usuario['cep'] ?? '';
+    $estado = $usuario['estado'] ?? '';
+    $cidade = $usuario['cidade'] ?? '';
+    $bairro = $usuario['bairro'] ?? '';
+    $rua = $usuario['rua'] ?? '';
+    $numero = $usuario['numero'] ?? '';
 ?>
 
 <!DOCTYPE html>
@@ -36,16 +62,77 @@
     <main class="main-pgConfigProfile">
         <section class="sectionDadosCadastrais-pgConfigProfile">
             <p>Mostra dados sensiveis como nome de usuário, senha, email e etc... e dá a opção de alterar</p>
+            <form action= "<?php echo BASE_URL; ?> /sistema/atualizar_dados_pessoais.php" method="POST">
+    
+                <!-- necessário para identificar qual registro atualizar -->
+                <label>Nome:</label><br>
+                <input type="text" name="nome" value="<?php echo $nome ?>" required><br><br>
+
+                <label>Username:</label><br>
+                <input type="text" name="nome" value="<?php echo $username ?>" required><br><br>
+                
+                <label>RG:</label><br>
+                <input type="text" name="telefone" value="<?php echo $rg ?>" required><br><br>
+                
+                <label>CPF:</label><br>
+                <input type="text" name="telefone" value="<?php echo $cpf ?>" required><br><br>
+                
+                <label>email:</label><br>
+                <input type="text" name="telefone" value="<?php echo $email ?>" required><br><br>
+                
+                <label>Telefone:</label><br>
+                <input type="text" name="telefone" value="<?php echo $telefone ?>" required><br><br>
+                
+                <label>Data de nascimento:</label><br>
+                <input type="date" name="telefone" value="<?php echo $datanasc ?>" required><br><br>
+
+                <label>Senha Atual:</label><br>
+                <input type="email" name="email" placeholder="Digite sua senha" required><br><br>
+
+                <label>Nova Senha:</label><br>
+                <input type="email" name="email" placeholder="Digite uma nova senha" required><br><br>
+
+                <button type="submit">Atualizar</button>
+            </form>
         </section>
 
         <section class="sectionLocal-pgConfigProfile">
             <p>mosta dados do endereço cadastrado e da a opção de alterar</p>
+            <form action= "<?php echo BASE_URL; ?> /sistema/atualizar_dados_local.php" method="POST">
+    
+                <!-- necessário para identificar qual registro atualizar -->
+                <label>CEP:</label><br>
+                <input type="text" name="nome" value="<?php echo $cep ?>" required><br><br>
+
+                <label>Estado (UF):</label><br>
+                <input type="text" name="nome" value="<?php echo $estado ?>" required><br><br>
+
+                <label>Cidade:</label><br>
+                <input type="email" name="email" value="<?php echo $cidade ?>" required><br><br>
+
+                <label>Bairro:</label><br>
+                <input type="email" name="email" value="<?php echo $bairro ?>" required><br><br>
+
+                <label>Rua:</label><br>
+                <input type="text" name="telefone" value="<?php echo $rua ?>" required><br><br>
+                
+                <label>Número:</label><br>
+                <input type="text" name="telefone" value="<?php echo $numero ?>" required><br><br>
+                
+                <label>Senha:</label><br>
+                <input type="text" name="telefone" placeholder="Digite sua senha" required><br><br>
+
+                <button type="submit">Atualizar</button>
+            </form>
         </section>
 
         <section class="sectionMaisOpcoes-pgConfigProfile">
             <h2>Ações da Conta</h2>
             <form method="POST">
                 <button type="submit" name="btnLogOut" value="fazerLogout">Fazer Logout</button>
+            </form>
+            <form method="POST">
+                <button type="submit" name="btnExcluirConta" value="excluirConta">Excluir conta</button>
             </form>
 
             <?php
