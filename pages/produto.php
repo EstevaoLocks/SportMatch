cabcae895f0d028d27de0c86fe803b337a697bec
estevaolocks <estevao.locks@gmail.com>
2025-11-24 08:09:58 -0300

    atualizações

--- pages/produto.php
+++ pages/produto.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__ . '/../sitema/classes/config.php';
-    require_once __DIR__ . '/../sitema/classes/conexao.php';
+    require_once __DIR__ . '/../../conexao.php';
 
     if (session_status() === PHP_SESSION_NONE) session_start();
 
