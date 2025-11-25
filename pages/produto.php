2acecf6581bd98591420d929705b22e391285baf
EstevaoLocks <estevao.locks11@gmail.com>
2025-11-24 23:39:10 -0300

    ajuste

--- pages/produto.php
+++ pages/produto.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__ . '/../config.php';
-    require_once BASE_PATH . '/conexao.php';
+    require_once BASE_PATH . '/sistema/conexao.php';
 
 
     if (!isset($_GET['id'])) {
